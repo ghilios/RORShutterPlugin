870c0c88610b59e8b4a4c6c28ee251b847b94f78
Stefan Berg <isbeorn86@googlemail.com>
2021-06-17 22:22:23 +0200

    Adjust plugin template to latest version and add additional comments

--- NINA.Plugin.Template/NINA.Plugin.Template/MyPlugin.cs
+++ NINA.Plugin.Template/NINA.Plugin.Template/MyPlugin.cs
@@ -9,9 +9,11 @@ using System.Threading.Tasks;
 
 namespace MyPluginNamespace {
     /// <summary>
-    /// This class exports the IPlugin interface and will be used for the general plugin information and options
+    /// This class exports the IPluginManifest interface and will be used for the general plugin information and options
+    /// The base class "PluginBase" will populate all the necessary Manifest Meta Data out of the AssemblyInfo attributes. Please fill these accoringly
+    /// 
     /// An instance of this class will be created and set as datacontext on the plugin options tab in N.I.N.A. to be able to configure global plugin settings
-    /// The user interface for the settings will be defined in the Options.xaml
+    /// The user interface for the settings will be defined by a DataTemplate with the key "<MyPlugin.Name>_Options" - In this template example it is found in the Options.xaml
     /// </summary>
     [Export(typeof(IPluginManifest))]
     public class MyPlugin : PluginBase {
--- NINA.Plugin.Template/NINA.Plugin.Template/Properties/AssemblyInfo.cs
+++ NINA.Plugin.Template/NINA.Plugin.Template/Properties/AssemblyInfo.cs
@@ -2,51 +2,69 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// The name of your plugin
+// [MANDATORY] The following GUID is used as a unique identifier of the plugin
+[assembly: Guid("141704e0-505c-492c-9e74-3b85f1c05d12")]
+
+// [MANDATORY] The assembly versioning
+//Should be incremented for each new release build of a plugin
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+
+// [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("MyPlugin")]
-// A short description of your plugin
+// [MANDATORY] A short description of your plugin
 [assembly: AssemblyDescription("This is the description of my plugin. It should contain all relevant description of what the plugin is about.")]
-[assembly: AssemblyConfiguration("")]
-//Your name
+
+
+// The following attributes are not required for the plugin per se, but are required by the official manifest meta data
+
+// Your name
 [assembly: AssemblyCompany("MyCompanyOrName")]
-//The product name that this plugin is part of
-[assembly: AssemblyProduct("NINA.Plugin.Template")]
+// The product name that this plugin is part of
+[assembly: AssemblyProduct("MyPluginProduct")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is used as a unique identifier of the plugin
-[assembly: Guid("141704e0-505c-492c-9e74-3b85f1c05d12")]
-
-//The assembly versioning
-//Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
-
-//The minimum Version of N.I.N.A. that this plugin is compatible with
+// The minimum Version of N.I.N.A. that this plugin is compatible with
 [assembly: AssemblyMetadata("MinimumApplicationVersion", "1.11.0.1092")]
 
-//Your plugin homepage - omit if not applicaple
-[assembly: AssemblyMetadata("Homepage", "https://example.com/")]
-//The license your plugin code is using
+// The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
-//The url to the license
+// The url to the license
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
-//The repository where your pluggin is hosted
+// The repository where your pluggin is hosted
 [assembly: AssemblyMetadata("Repository", "https://bitbucket.org/Isbeorn/nina.plugin.template/")]
 
-//Common tags that quickly describe your plugin
+
+// The following attributes are optional for the official manifest meta data
+
+//[Optional] Your plugin homepage - omit if not applicaple
+[assembly: AssemblyMetadata("Homepage", "https://example.com/")]
+
+//[Optional] Common tags that quickly describe your plugin
 [assembly: AssemblyMetadata("Tags", "Template,Sequencer")]
 
-//The featured logo that will be displayed in the plugin list next to the name
+//[Optional] A link that will show a log of all changes in between your plugin's versions
+[assembly: AssemblyMetadata("ChangelogURL", "https://bitbucket.org/Isbeorn/nina.plugin.template/commits/branch/master")]
+
+//[Optional] The featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "https://nighttime-imaging.eu/wp-content/uploads/2019/02/Logo_Nina.png")]
-//An example screenshot of your plugin in action
+//[Optional] An example screenshot of your plugin in action
 [assembly: AssemblyMetadata("ScreenshotURL", "https://bitbucket.org/Isbeorn/nina.plugins/downloads/Starlock2.png")]
-//An additional example screenshot of your plugin in action
+//[Optional] An additional example screenshot of your plugin in action
 [assembly: AssemblyMetadata("AltScreenshotURL", "https://bitbucket.org/Isbeorn/nina.plugins/downloads/Instruction.png")]
+//[Optional] An in-depth description of your plugin
 [assembly: AssemblyMetadata("LongDescription", @"An in-depth description of your plugin")]
+
+
+
+// [Unused]
+[assembly: AssemblyConfiguration("")]
+// [Unused]
+[assembly: AssemblyTrademark("")]
+// [Unused]
+[assembly: AssemblyCulture("")]
